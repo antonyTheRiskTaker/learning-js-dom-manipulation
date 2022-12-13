a82a961311728ff4c2c58cb6f1ea6e78e7111864
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-12-13 22:58:13 +0800

    added comments as reminder

--- app.js
+++ app.js
@@ -1,32 +1,9 @@
-// DOM Manipulation
+// DOM manipulation
 
-// Creating elements
-const ul = document.querySelector('ul');
-const li = document.createElement('li');
+// Traverse the DOM
 
-// Adding elements
-ul.append(li);
+// Parent node traversal
 
-// Modifying the text
+// Child node traversal
 
-const firstListItem = document.querySelector('.list-items');
-
-// All three methods are similar, but with nuances.
-// console.log(firstListItem.innerText);
-// console.log(firstListItem.textContent);
-// console.log(firstListItem.innerHTML); //! Presents potential security risk.
-
-li.innerText = 'X-men';
-
-// Modifying attributes & classes
-// li.setAttribute('id', 'main-heading');
-// li.removeAttribute('id');
-
-// const title = document.querySelector('#main-heading');
-
-// console.log(title.getAttribute('id'));
-
-li.classList.add('list-items');
-li.classList.remove('list-items');
-
-console.log(li.classList.contains('list-items'));
+// Sibling node traversal
